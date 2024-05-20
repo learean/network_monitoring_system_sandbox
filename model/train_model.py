3bab02afe8eefd04f190959d0ca8bc413e7b9ad8
Vitalii <vitalybilyi@gmail.com>
2024-05-20 23:37:42 +0100

    update

--- model/train_model.py
+++ model/train_model.py
@@ -1,16 +1,12 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, accuracy_score
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.callbacks import EarlyStopping
 
 # Load and preprocess the dataset
 flow_dataset = pd.read_csv('dataset.csv')
 
-# Replace dots in specific columns
 flow_dataset.iloc[:, 2] = flow_dataset.iloc[:, 2].str.replace('.', '')
 flow_dataset.iloc[:, 3] = flow_dataset.iloc[:, 3].str.replace('.', '')
 flow_dataset.iloc[:, 5] = flow_dataset.iloc[:, 5].str.replace('.', '')
@@ -20,49 +16,36 @@ X_flow = X_flow.astype('float64')
 
 y_flow = flow_dataset.iloc[:, -1].values
 
+# Split the data
 X_flow_train, X_flow_test, y_flow_train, y_flow_test = train_test_split(X_flow, y_flow, test_size=0.25, random_state=0)
 
-# Build the enhanced neural network
-model = Sequential([
-    Dense(128, input_dim=X_flow_train.shape[1], activation='relu'),
-    Dropout(0.5),
-    Dense(64, activation='relu'),
-    Dropout(0.5),
-    Dense(32, activation='relu'),
-    Dense(1, activation='sigmoid')
+# Scale the data
+scaler = StandardScaler()
+X_flow_train = scaler.fit_transform(X_flow_train)
+X_flow_test = scaler.transform(X_flow_test)
+
+# Build the MLP model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(X_flow_train.shape[1],)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
+# Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Early stopping to avoid overfitting
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-
-# Convert training data to a TensorFlow dataset and repeat it
-train_dataset = tf.data.Dataset.from_tensor_slices((X_flow_train, y_flow_train))
-train_dataset = train_dataset.shuffle(buffer_size=1024).batch(32).repeat()
-
-# Convert validation data to a TensorFlow dataset (no need to repeat)
-val_dataset = tf.data.Dataset.from_tensor_slices((X_flow_test, y_flow_test))
-val_dataset = val_dataset.batch(32)
-
-# Calculate the number of steps per epoch
-steps_per_epoch = len(X_flow_train) // 32
-
-# Train the model with early stopping
-model.fit(train_dataset, epochs=5, steps_per_epoch=steps_per_epoch, validation_data=val_dataset, callbacks=[early_stopping], verbose=1)
+# Train the model
+model.fit(X_flow_train, y_flow_train, epochs=10, batch_size=32, validation_split=0.1)
 
 # Evaluate the model
 y_flow_pred = (model.predict(X_flow_test) > 0.5).astype("int32")
 
-print("Confusion matrix")
 cm = confusion_matrix(y_flow_test, y_flow_pred)
-print(cm)
-
 acc = accuracy_score(y_flow_test, y_flow_pred)
-print("Success accuracy = {0:.2f} %".format(acc * 100))
-fail = 1.0 - acc
-print("Fail accuracy = {0:.2f} %".format(fail * 100))
+
+print("Confusion Matrix:")
+print(cm)
+print("Accuracy: {:.2f}%".format(acc * 100))
 
 # Save the model
-model.save('flow_model.h5')
-print("Model saved to flow_model.h5")
+model.save('flow_mlp_model.h5')
--- ryu/controller.py
+++ ryu/controller.py
@@ -7,9 +7,11 @@ from ryu.lib import hub
 
 import switch
 from datetime import datetime
+
 import pandas as pd
-import numpy as np
 import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+
 
 class SimpleMonitor13(switch.SimpleSwitch13):
 
@@ -20,13 +22,12 @@ class SimpleMonitor13(switch.SimpleSwitch13):
 
         start = datetime.now()
 
-        self.load_model()
+        self.flow_training()
 
         end = datetime.now()
-        print("Model loading time: ", (end - start))
+        print("Training time: ", (end - start))
 
-    @set_ev_cls(ofp_event.EventOFPStateChange,
-                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
@@ -43,7 +44,6 @@ class SimpleMonitor13(switch.SimpleSwitch13):
             for dp in self.datapaths.values():
                 self._request_stats(dp)
             hub.sleep(10)
-
             self.flow_predict()
 
     def _request_stats(self, datapath):
@@ -55,68 +55,76 @@ class SimpleMonitor13(switch.SimpleSwitch13):
 
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
-        timestamp = datetime.now().timestamp()
+        timestamp = datetime.now()
+        timestamp = timestamp.timestamp()
 
-        with open("PredictFlowStatsFile.csv", "w") as file0:
-            file0.write('timestamp,datapath_id,flow_id,ip_src,tp_src,ip_dst,tp_dst,ip_proto,icmp_code,icmp_type,flow_duration_sec,flow_duration_nsec,idle_timeout,hard_timeout,flags,packet_count,byte_count,packet_count_per_second,packet_count_per_nsecond,byte_count_per_second,byte_count_per_nsecond\n')
-            body = ev.msg.body
-            icmp_code = -1
-            icmp_type = -1
-            tp_src = 0
-            tp_dst = 0
+        file0 = open("stats_file.csv", "w")
+        file0.write(
+            'timestamp,datapath_id,flow_id,ip_src,tp_src,ip_dst,tp_dst,ip_proto,icmp_code,icmp_type,flow_duration_sec,flow_duration_nsec,idle_timeout,hard_timeout,flags,packet_count,byte_count,packet_count_per_second,packet_count_per_nsecond,byte_count_per_second,byte_count_per_nsecond\n')
+        body = ev.msg.body
+        icmp_code = -1
+        icmp_type = -1
+        tp_src = 0
+        tp_dst = 0
 
-            for stat in sorted([flow for flow in body if (flow.priority == 1)], key=lambda flow:
-            (flow.match['eth_type'], flow.match['ipv4_src'], flow.match['ipv4_dst'], flow.match['ip_proto'])):
+        for stat in sorted([flow for flow in body if (flow.priority == 1)], key=lambda flow:
+        (flow.match['eth_type'], flow.match['ipv4_src'], flow.match['ipv4_dst'], flow.match['ip_proto'])):
 
-                ip_src = stat.match['ipv4_src']
-                ip_dst = stat.match['ipv4_dst']
-                ip_proto = stat.match['ip_proto']
+            ip_src = stat.match['ipv4_src']
+            ip_dst = stat.match['ipv4_dst']
+            ip_proto = stat.match['ip_proto']
 
-                if stat.match['ip_proto'] == 1:
-                    icmp_code = stat.match['icmpv4_code']
-                    icmp_type = stat.match['icmpv4_type']
+            if stat.match['ip_proto'] == 1:
+                icmp_code = stat.match['icmpv4_code']
+                icmp_type = stat.match['icmpv4_type']
 
-                elif stat.match['ip_proto'] == 6:
-                    tp_src = stat.match['tcp_src']
-                    tp_dst = stat.match['tcp_dst']
+            elif stat.match['ip_proto'] == 6:
+                tp_src = stat.match['tcp_src']
+                tp_dst = stat.match['tcp_dst']
 
-                elif stat.match['ip_proto'] == 17:
-                    tp_src = stat.match['udp_src']
-                    tp_dst = stat.match['udp_dst']
+            elif stat.match['ip_proto'] == 17:
+                tp_src = stat.match['udp_src']
+                tp_dst = stat.match['udp_dst']
 
-                flow_id = str(ip_src) + str(tp_src) + str(ip_dst) + str(tp_dst) + str(ip_proto)
+            flow_id = str(ip_src) + str(tp_src) + str(ip_dst) + str(tp_dst) + str(ip_proto)
 
-                try:
-                    packet_count_per_second = stat.packet_count / stat.duration_sec
-                    packet_count_per_nsecond = stat.packet_count / stat.duration_nsec
-                except:
-                    packet_count_per_second = 0
-                    packet_count_per_nsecond = 0
+            try:
+                packet_count_per_second = stat.packet_count / stat.duration_sec
+                packet_count_per_nsecond = stat.packet_count / stat.duration_nsec
+            except:
+                packet_count_per_second = 0
+                packet_count_per_nsecond = 0
 
-                try:
-                    byte_count_per_second = stat.byte_count / stat.duration_sec
-                    byte_count_per_nsecond = stat.byte_count / stat.duration_nsec
-                except:
-                    byte_count_per_second = 0
-                    byte_count_per_nsecond = 0
+            try:
+                byte_count_per_second = stat.byte_count / stat.duration_sec
+                byte_count_per_nsecond = stat.byte_count / stat.duration_nsec
+            except:
+                byte_count_per_second = 0
+                byte_count_per_nsecond = 0
 
-                file0.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n"
-                            .format(timestamp, ev.msg.datapath.id, flow_id, ip_src, tp_src, ip_dst, tp_dst,
-                                    stat.match['ip_proto'], icmp_code, icmp_type,
-                                    stat.duration_sec, stat.duration_nsec,
-                                    stat.idle_timeout, stat.hard_timeout,
-                                    stat.flags, stat.packet_count, stat.byte_count,
-                                    packet_count_per_second, packet_count_per_nsecond,
-                                    byte_count_per_second, byte_count_per_nsecond))
+            file0.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n"
+                        .format(timestamp, ev.msg.datapath.id, flow_id, ip_src, tp_src, ip_dst, tp_dst,
+                                stat.match['ip_proto'], icmp_code, icmp_type,
+                                stat.duration_sec, stat.duration_nsec,
+                                stat.idle_timeout, stat.hard_timeout,
+                                stat.flags, stat.packet_count, stat.byte_count,
+                                packet_count_per_second, packet_count_per_nsecond,
+                                byte_count_per_second, byte_count_per_nsecond))
 
-    def load_model(self):
-        self.logger.info("Loading neural network model ...")
-        self.flow_model = tf.keras.models.load_model(os.path.join('../model/flow_model.h5'))
+        file0.close()
+
+    def flow_training(self):
+        self.logger.info("Flow Training ...")
+
+        # Load the trained MLP model
+        self.flow_model = tf.keras.models.load_model(os.path.join('../model/flow_mlp_model.h5'))
+        self.scaler = StandardScaler()
+
         self.logger.info("Model loaded successfully")
 
     def flow_predict(self):
         try:
-            predict_flow_dataset = pd.read_csv('PredictFlowStatsFile.csv')
+            predict_flow_dataset = pd.read_csv('stats_file.csv')
 
             predict_flow_dataset.iloc[:, 2] = predict_flow_dataset.iloc[:, 2].str.replace('.', '')
             predict_flow_dataset.iloc[:, 3] = predict_flow_dataset.iloc[:, 3].str.replace('.', '')
@@ -125,23 +133,33 @@ class SimpleMonitor13(switch.SimpleSwitch13):
             X_predict_flow = predict_flow_dataset.iloc[:, :].values
             X_predict_flow = X_predict_flow.astype('float64')
 
+            X_predict_flow = self.scaler.transform(X_predict_flow)
+
             y_flow_pred = (self.flow_model.predict(X_predict_flow) > 0.5).astype("int32")
 
-            legitimate_traffic = np.sum(y_flow_pred == 0)
-            ddos_traffic = np.sum(y_flow_pred == 1)
+            legitimate_trafic = 0
+            ddos_trafic = 0
+
+            for i in y_flow_pred:
+                if i == 0:
+                    legitimate_trafic += 1
+                else:
+                    ddos_trafic += 1
+                    victim = int(predict_flow_dataset.iloc[i, 5]) % 20
 
             self.logger.info("------------------------------------------------------------------------------")
-            if (legitimate_traffic / len(y_flow_pred) * 100) > 80:
-                self.logger.info("Legitimate traffic ...")
+            if (legitimate_trafic / len(y_flow_pred) * 100) > 80:
+                self.logger.info("legitimate traffic ...")
             else:
-                self.logger.info("DDoS traffic ...")
-                victims = predict_flow_dataset.iloc[y_flow_pred.flatten() == 1, 5].values
-                victims = [int(victim) % 20 for victim in victims]
-                unique_victims = set(victims)
-                self.logger.info("Victims are hosts: {}".format(', '.join(f'h{victim}' for victim in unique_victims)))
+                self.logger.info("ddos traffic ...")
+                self.logger.info("victim is host: h{}".format(victim))
+
             self.logger.info("------------------------------------------------------------------------------")
 
-            with open("PredictFlowStatsFile.csv", "w") as file0:
-                file0.write('timestamp,datapath_id,flow_id,ip_src,tp_src,ip_dst,tp_dst,ip_proto,icmp_code,icmp_type,flow_duration_sec,flow_duration_nsec,idle_timeout,hard_timeout,flags,packet_count,byte_count,packet_count_per_second,packet_count_per_nsecond,byte_count_per_second,byte_count_per_nsecond\n')
+            file0 = open("stats_file.csv", "w")
+            file0.write(
+                'timestamp,datapath_id,flow_id,ip_src,tp_src,ip_dst,tp_dst,ip_proto,icmp_code,icmp_type,flow_duration_sec,flow_duration_nsec,idle_timeout,hard_timeout,flags,packet_count,byte_count,packet_count_per_second,packet_count_per_nsecond,byte_count_per_second,byte_count_per_nsecond\n')
+            file0.close()
+
         except Exception as e:
-            self.logger.error("Error in flow prediction: {}".format(e))
+            self.logger.error(f"Error in flow prediction: {e}")
